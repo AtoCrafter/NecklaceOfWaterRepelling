16a5b4811326318efcff07217770710007f77ac1
AtoCrafter <ato.crafter@gmail.com>
2015-01-09 19:34:55 +0900

    Add Recipe

--- src/main/scala/ato/necklaceofwaterrepelling/NecklaceOfWaterRepelling.scala
+++ src/main/scala/ato/necklaceofwaterrepelling/NecklaceOfWaterRepelling.scala
@@ -5,6 +5,8 @@ import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.registry.GameRegistry
+import net.minecraft.init.Items
+import net.minecraft.item.ItemStack
 
 @Mod(modid = "NecklaceOfWaterRepelling", modLanguage = "scala")
 object NecklaceOfWaterRepelling {
@@ -14,5 +16,6 @@ object NecklaceOfWaterRepelling {
   @EventHandler
   def preInit(event: FMLPreInitializationEvent): Unit = {
     GameRegistry.registerItem(itemNecklace, "NecklaceOfWaterRepelling")
+    GameRegistry.addShapelessRecipe(new ItemStack(itemNecklace), Items.nether_star, Items.bucket)
   }
 }
