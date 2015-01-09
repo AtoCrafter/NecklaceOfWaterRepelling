803949f8dbe26aef9039b6ab1e86a063d771c8a3
AtoCrafter <ato.crafter@gmail.com>
2015-01-09 19:31:25 +0900

    Remote TempWater

--- src/main/scala/ato/necklaceofwaterrepelling/NecklaceOfWaterRepelling.scala
+++ src/main/scala/ato/necklaceofwaterrepelling/NecklaceOfWaterRepelling.scala
@@ -1,6 +1,5 @@
 package ato.necklaceofwaterrepelling
 
-import ato.necklaceofwaterrepelling.block.BlockTempWater
 import ato.necklaceofwaterrepelling.item.ItemNecklaceOfWaterRepelling
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
@@ -10,12 +9,10 @@ import cpw.mods.fml.common.registry.GameRegistry
 @Mod(modid = "NecklaceOfWaterRepelling", modLanguage = "scala")
 object NecklaceOfWaterRepelling {
 
-  val blockTemp = new BlockTempWater()
   val itemNecklace = new ItemNecklaceOfWaterRepelling()
 
   @EventHandler
   def preInit(event: FMLPreInitializationEvent): Unit = {
-    GameRegistry.registerBlock(blockTemp, "TempWater")
     GameRegistry.registerItem(itemNecklace, "NecklaceOfWaterRepelling")
   }
 }
--- src/main/scala/ato/necklaceofwaterrepelling/item/ItemNecklaceOfWaterRepelling.scala
+++ src/main/scala/ato/necklaceofwaterrepelling/item/ItemNecklaceOfWaterRepelling.scala
@@ -2,8 +2,11 @@ package ato.necklaceofwaterrepelling.item
 
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.entity.Entity
+import net.minecraft.init.Blocks
 import net.minecraft.item.{Item, ItemStack}
 import net.minecraft.world.World
+
+import scala.math._
 
 class ItemNecklaceOfWaterRepelling extends Item {
 
@@ -11,6 +14,16 @@ class ItemNecklaceOfWaterRepelling extends Item {
   setCreativeTab(CreativeTabs.tabCombat)
 
   override def onUpdate(itemstack: ItemStack, world: World, entity: Entity, p_77663_4_ : Int, p_77663_5_ : Boolean): Unit = {
+    val (x, y, z) = (floor(entity.posX).toInt, floor(entity.posY).toInt, floor(entity.posZ).toInt)
+    val d = 5
+    for (dx <- x - d to x + d; dy <- y - d to y + d; dz <- z - d to z + d
+         if (dx - x) * (dx - x) + (dy - y) * (dy - y) + (dz - z) * (dz - z) < d * d) {
+      world.getBlock(dx, dy, dz) match {
+        case Blocks.flowing_water => world.setBlockToAir(dx, dy, dz)
+        case Blocks.water => world.setBlockToAir(dx, dy, dz)
+        case _ =>
+      }
+    }
   }
 
 }
